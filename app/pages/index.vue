aad61871b10bae27e57b5a0021a0784b98a1164a
Николай <nickneustroev2@gmail.com>
2026-06-13 19:00:20 +0700

    123

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,7 +5,6 @@ import BarChartBasic from '~/components/Demo/BarChartBasic.vue'
 <template>
   <div>
     <BarChartBasic />
-    <DemoLineChart />
     <UPageHero
       title="Nuxt Starter Template"
       description="A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours."
