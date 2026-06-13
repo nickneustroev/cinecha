36e8724101bf0f95fd547ab8ae5f253081ff1cb6
Николай <nickneustroev2@gmail.com>
2026-06-13 18:37:29 +0700

    ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
     'nuxt-charts'
   ],
+  ssr: false,
 
   devtools: {
     enabled: true
