b03fe79c750c95774e5ea1f7014f73d79b35500f
Николай <nickneustroev2@gmail.com>
2026-06-16 16:24:24 +0700

    revert: back to recommended file+langDir approach, remove i18n.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,10 +11,9 @@ export default defineNuxtConfig({
   i18n: {
     defaultLocale: 'ru',
     strategy: 'no_prefix',
-    vueI18n: './i18n.config.ts',
     locales: [
-      { code: 'en', name: 'English' },
-      { code: 'ru', name: 'Русский' }
+      { code: 'en', name: 'English', file: 'en.json' },
+      { code: 'ru', name: 'Русский', file: 'ru.json' }
     ],
     detectBrowserLanguage: false
   },
